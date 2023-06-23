7f319b8bdb4b235d8ed0941bf8ba7b6c41032bd5
Reinhold <8436725+reikoch@users.noreply.github.com>
2023-06-23 16:34:52 +0200

    add test dataframe and test

--- inst/tinytest/test_smetlite.R
+++ inst/tinytest/test_smetlite.R
@@ -1,4 +1,4 @@
-
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
-
+# Check with simple test SMET file
+xx <- read_smet(system.file('extdata', 'test.smet', package = 'smetlite'))
+attr(xx, 'file') <- 'test.smet'
+expect_equal(xx, test_smet)
